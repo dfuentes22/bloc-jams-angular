ee23c473772d839596cb694e4f24fa72107a6dbc
Dan <dvf5104@gmail.com>
2015-10-05 19:41:16 -0400

    Added templates and state and location providers

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,1 +1,27 @@
-var blocJams = angular.module('blocJams', []);
+var blocJams = angular.module('blocJams', ['ui.router']);
+
+myAppModule.config(function($stateProvider, $locationProvider) {
+ 
+    $locationProvider.html5Mode({
+    enabled: true,
+    requireBase: false
+    });
+    
+    $stateProvider
+        .state('landing', {
+            url: '/landing',
+            controller: 'Landing.controller',
+            templateUrl: '/templates/landing.html'
+        })
+        .state('collection', {
+            url: '/collection',
+            controller: 'Collection.controller',
+            templateUrl: '/templates/collection.html'
+        })
+        .state('album', {
+            url: '/album',
+            controller: 'Album.controller',
+            templateUrl: '/templates/album.html'
+        })
+
+});
